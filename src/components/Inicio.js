0b5cf68b79dbc79c640e877cf625dce86b89041f
victor_perezvaldez@hotmail.com <victor_perezvaldez@hotmail.com>
2022-06-19 00:48:50 -0500

    Listo ahora si, faltaba algo mas de responsive

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -10,8 +10,8 @@ export const Inicio = () => {
             <img src={restaurante}/>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sodales dui sit amet turpis cursus, vel ornare felis fermentum. Nullam fringilla, odio non condimentum blandit, velit sem tempor est, sed bibendum leo neque quis ligula. Maecenas ut purus accumsan, cursus neque sed, molestie dolor. Morbi nec erat sit amet ante imperdiet tincidunt. Integer ac pellentesque massa. Nulla a elit leo. Etiam ac vestibulum augue. Aliquam quis porttitor dolor. Quisque porttitor, ex sit amet dapibus aliquam, urna ipsum aliquet tortor, vitae lacinia leo eros a lectus. Praesent vel tortor scelerisque, commodo metus in, semper nulla. Nam laoreet neque ut lorem volutpat, sit amet placerat elit feugiat. Nullam ac interdum metus, sed convallis ex. Etiam molestie at ex porta egestas.
             </p> 
-           <Link to='/reservaciones' className='btn btn-primaryColor-inicio'>Hacer una Reservacion</Link>
         </div>
+      <Link to='/reservaciones' className='btn btn-primaryColor-inicio'>Hacer una Reservacion</Link>
 
     </div>
   )
